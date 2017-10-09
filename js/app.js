fa27130454ae81f3d6260e747e73d177acf514ea
edgar <spickerman.edgar@gmail.com>
2017-10-09 13:34:55 -0700

    broke the code down further

--- js/app.js
+++ js/app.js
@@ -3,20 +3,60 @@
     let htmlData = [];
 
     //functions
-    function attach(spot,content) {
-        $(spot).append(content);
-    }
+    const attach = (spot, content) => $(spot).append(content);
+
+    function createSearch(data, i) {
+        let searchResults = {};
+        searchResults.html = createDir(data, i);
+        searchResults.name = data.results[i].name.first + ' ' + data.results[i].name.last;
+        searchResults.username = data.results[i].login.username;
+        htmlData.push(searchResults)
+    } //creates a search object to be searched thru if a user clicks search
+
+    function createDir(data, i) {
+        let html = '<div class="employee ' + i + ' border"><img class="photo"src="' + data.results[i].picture.large + '"/>';
+        html += '<div class="info"><p class="name">' + data.results[i].name.first + ' ' + data.results[i].name.last + '</p>';
+        html += '<p class="email">' + data.results[i].email + '</li>';
+        html += '<p class="location">' + data.results[i].location.city + ', ' + data.results[i].location.state + '</p></div></div>';
+        attach('#directory', html);
+        return html;
+    }//creates the directory and attaches it to the main page after a successful ajax call
+
+    function createOverlay(data, i) {
+        let year = data.results[i].dob.slice(0, 4);
+        let month = data.results[i].dob.slice(5, 7);
+        let day = data.results[i].dob.slice(8, 10);
+        let overlay = '<div class="modual border"><img  class="overlay photo photo-lrg" src="' + data.results[i].picture.large + '">';
+        overlay += '<p class="overlay name">' + data.results[i].name.first + ' ' + data.results[i].name.last + '</p>';
+        overlay += '<p class="overlay username">' + data.results[i].login.username + '</p>';
+        overlay += '<p class="overlay email btm-border">' + data.results[i].email + '</p>';
+        overlay += '<p class="overlay cell">' + data.results[i].cell + '</p>';
+        overlay += '<p class="overlay address">' + data.results[i].location.street + ' ' + data.results[i].location.city + ', ';
+        overlay += data.results[i].location.state + ' ' + data.results[i].location.postcode + '</p>';
+        overlay += '<p class="overlay-dob">Birthday: ' + month + '/' + day + '/' + year + '</p><a class="close">X</a>';
+        overlay += '<a class="previous"><</a><a class="next">></a></div>';
+        overlayArray.push(overlay);
+    } //creates the overlay Array with all the properties of the employees
+
+    function employeeOverlay() {
+        let currentLink = this.classList[1];
+        $('#overlay').html(overlayArray[this.classList[1]]);
+        $('#overlay').css('display', 'block');
+        $('#overlay').click((e) => e.target.id === 'overlay' ? $('#overlay').css('display', 'none') : '');
+        createPagination(currentLink);
+    } //displays the modual window and sets the properties to the clicked employee
+
     function createPagination(currentLink) {
-        let filteredArray = [];
+        let displayedArray = [];
         for (var i = 0; i < $('.employee').length; i++) {
-            filteredArray.push($('.employee')[i].classList[1])
+            displayedArray.push($('.employee')[i].classList[1])
         } //creates a new array housing the displayed employee numbers
-        for (var j = 0; j < filteredArray.length; j++){
-            if (filteredArray[j] === currentLink) {
+        for (var j = 0; j < displayedArray.length; j++){
+            if (displayedArray[j] === currentLink) {
                 let currentPos = j;
                 $('a.close').click(() => $('#overlay').css('display', 'none'));
-                $('a.previous').click(() => previous(currentPos, filteredArray));
-                $('a.next').click(() => next(currentPos, filteredArray));
+                $('a.previous').click(() => previous(currentPos, displayedArray));
+                $('a.next').click(() => next(currentPos, displayedArray));
             }
         }//locates the clicked employee's location in the new array housing created by the filter calls navigation
     }
@@ -32,14 +72,6 @@
         $('#overlay').html(overlayArray[arr[newLink]]);
         createPagination(arr[newLink])
     } //finds the next employee resets if its the last employee,displays the overlay for that employee,creates navigation
-
-    function employeeOverlay() {
-        let currentLink = this.classList[1];
-        $('#overlay').html(overlayArray[this.classList[1]]);
-        $('#overlay').css('display', 'block');
-        $('#overlay').click((e) => e.target.id === 'overlay' ? $('#overlay').css('display', 'none') : '');
-        createPagination(currentLink);
-    } //gets which employee fired the event, finds his location,opens up the modual,closes when user clicks outside modual,creates navigation.
 
     //events
     $('.searchbtn').click(() => {
@@ -58,32 +90,9 @@
         dataType: 'json',
         success: function (data) {
             for (var i = 0; i < 12; i++) {
-                let searchResults = {};
-                let overlay = '';
-                let html = '';
-                html += '<div class="employee ' + i + ' border"><img class="photo"src="' + data.results[i].picture.large + '"/>';
-                html += '<div class="info"><p class="name">' + data.results[i].name.first + ' ' + data.results[i].name.last + '</p>';
-                html += '<p class="email">' + data.results[i].email + '</li>';
-                html += '<p class="location">' + data.results[i].location.city + ', ' + data.results[i].location.state + '</p></div></div>';
-                overlay += '<div class="modual border"><img  class="overlay photo photo-lrg" src="' + data.results[i].picture.large + '">';
-                overlay += '<p class="overlay name">' + data.results[i].name.first + ' ' + data.results[i].name.last + '</p>';
-                overlay += '<p class="overlay username">' + data.results[i].login.username + '</p>';
-                overlay += '<p class="overlay email btm-border">' + data.results[i].email + '</p>';
-                overlay += '<p class="overlay cell">' + data.results[i].cell + '</p>';
-                overlay += '<p class="overlay address">' + data.results[i].location.street +' '+ data.results[i].location.city + ', ';
-                overlay += data.results[i].location.state + ' ' + data.results[i].location.postcode + '</p>';
-                let year = data.results[i].dob.slice(0, 4);
-                let month = data.results[i].dob.slice(5, 7);
-                let day = data.results[i].dob.slice(8, 10);
-                overlay += '<p class="overlay-dob">Birthday: ' + month + '/' + day + '/' + year + '</p><a class="close">X</a>';
-                overlay += '<a class="previous"><</a><a class="next">></a></div>';
-                searchResults.name = data.results[i].name.first + ' ' + data.results[i].name.last;
-                searchResults.username = data.results[i].login.username;
-                searchResults.html = html;
-                htmlData.push(searchResults)
-                overlayArray.push(overlay);
-                attach('#directory', html);
-            } //parses the ajax response into html to print to page and creates an object to be searched thru
+                createSearch(data, i);
+                createOverlay(data, i);
+            } //takes the results from the ajax response and creates the html for the search,directory, and overlay.
             $('.employee').click(employeeOverlay);
         }
     });
